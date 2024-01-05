b68ba62f2347492b5a24b0a858b92eeea28db908
IT Sherlock Holmes <125506117+CleverHolmes@users.noreply.github.com>
2024-01-05 09:16:34 -0800

    Update Media.jsx

--- src/components/banner/Media.jsx
+++ src/components/banner/Media.jsx
@@ -14,29 +14,29 @@ const Media = () => {
       <div>
         <h2 className="text-base uppercase font-titleFont mb-4">Find me on</h2>
         <div className="flex gap-4">
-          <span className="bannerIcon">
-            <a
+           <a
               href="https://github.com/Kattlover99"
               target="_blank"
               rel="noopener noreferrer"
             >
+          <span className="bannerIcon">
               <SiGithub />
-            </a>
           </span>
+            </a>
+          <a href="mailto:albertobial0912@gmail.com">
           <span className="bannerIcon">
-            <a href="mailto:albertobial0912@gmail.com">
               <SiGmail />
-            </a>
           </span>
-          <span className="bannerIcon">
-            <a
+            </a>
+          <a
               href="https://discordapp.com/users/435454025702309888"
               target="_blank"
               rel="noopener noreferrer"
             >
+          <span className="bannerIcon">
               <SiDiscord />
-            </a>
           </span>
+            </a>
         </div>
       </div>
       <div>
