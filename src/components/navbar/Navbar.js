8282fe3bc6918aad79c4514a2ca9c276f27b3762
IT Sherlock Holmes <125506117+CleverHolmes@users.noreply.github.com>
2024-01-05 09:21:06 -0800

    Update Navbar.js

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -73,29 +73,29 @@ const Navbar = () => {
                   Find me in
                 </h2>
                 <div className="flex gap-4">
-                  <span className="bannerIcon">
-                    <a
+                  <a
                       href="https://github.com/kattlover99"
                       target="_blank"
                       rel="noopener noreferrer"
                     >
+                  <span className="bannerIcon">
                       <SiGithub />
-                    </a>
                   </span>
+                        </a>
+                        <a href="mailto:albertobial0912@gmail.com">
                   <span className="bannerIcon">
-                    <a href="mailto:albertobial0912@gmail.com">
                       <SiGmail />
-                    </a>
                   </span>
-                  <span className="bannerIcon">
-                    <a
+                        </a>
+                        <a
                       href="https://discordapp.com/users/435454025702309888"
                       target="_blank"
                       rel="noopener noreferrer"
                     >
+                  <span className="bannerIcon">
                       <SiDiscord />
-                    </a>
                   </span>
+                        </a>
                 </div>
               </div>
               <span
